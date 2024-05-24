70414e97fe8d6a3492f7a90717e4430063e12f3a
Dwayne Crooks <me@dwaynecrooks.com>
2024-05-24 16:22:03 -0400

    Initial commit

---
+++ tests/Test/Example.elm
@@ -0,0 +1,14 @@
+module Test.Example exposing (suite)
+
+import Expect
+import Test exposing (Test, describe, test)
+
+
+suite : Test
+suite =
+    describe "Example"
+        [ test "1 + 1 == 2" <|
+            \_ ->
+                (1 + 1)
+                    |> Expect.equal 2
+        ]
