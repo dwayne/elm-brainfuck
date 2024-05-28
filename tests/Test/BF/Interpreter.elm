f88e78e2e27978de321315a95abc3e270a4c57a8
Dwayne Crooks <me@dwaynecrooks.com>
2024-05-28 11:44:10 -0400

    Add more tests
    I adpated them from:
    
      - https://github.com/robinpokorny/elm-brainfuck/blob/1a2da979e65094b05e388d6037681775061dea80/tests/BrainfuckTests.elm
      - https://github.com/akheron/brainfuck/blob/4adc696e8a0d60096c371f45d577b461fcbff5a6/tests/Eval.elm

--- tests/Test/BF/Interpreter.elm
+++ tests/Test/BF/Interpreter.elm
@@ -1,5 +1,6 @@
 module Test.BF.Interpreter exposing (suite)
 
+import BF.Data.Byte as Byte exposing (Byte)
 import BF.Data.Machine as Machine
 import BF.Interpreter as I
 import Expect
@@ -10,7 +11,16 @@ suite : Test
 suite =
     describe "BF.Interpreter" <|
         List.map testRun
-            [ ( "++++-+++-++-++[>++++-+++-++-++<-]>."
+            [ ( "empty"
+              , []
+              , ""
+              )
+            , ( String.repeat 48 "+" ++ "."
+              , []
+              , "0"
+              )
+            , ( "++++-+++-++-++[>++++-+++-++-++<-]>."
+              , []
               , "@"
               )
             , ( """
@@ -20,18 +30,64 @@ suite =
                 <+++[>----<-]>.<<<<<+++[>+++++<-]>.
                 >>.+++.------.--------.>>+.
                 """
+              , []
               , "Hello, World!"
+              )
+            , ( """
+                Echo until byte(0) or cat
+
+                ,[.[-],]
+                """
+              , stringToInts "Brainfuck" ++ [ 0 ]
+              , "Brainfuck"
+              )
+            , ( """
+                Echo until byte(255)
+
+                ",+[-.,+]"
+                """
+              , stringToInts "Brainfuck" ++ [ 255 ]
+              , "Brainfuck"
+              )
+            , ( """
+                Multiply 2 numbers
+
+                ,>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.
+                """
+              , [ 7, 8 ]
+              , intToString 56
+              )
+            , ( """
+                Bubble sort
+
+                >>,[>>,]<<[[<<]>>>>[<<[>+<<+>-]>>[>+<<<<[->]>[<]>>-]<<<[[-]>>[>+<-]>>[<<<+>>>-]]>>[[<+>-]>>]<]<<[>>+<<-]<<]>>>>[.>>]
+                """
+              , stringToInts "dacbe" ++ [ 0 ]
+              , "abcde"
               )
             ]
 
 
-testRun : ( String, String ) -> Test
-testRun ( source, expected ) =
+stringToInts : String -> List Int
+stringToInts =
+    String.toList >> List.map Char.toCode
+
+
+intToString : Int -> String
+intToString =
+    Char.fromCode >> String.fromChar
+
+
+testRun : ( String, List Int, String ) -> Test
+testRun ( source, ints, expected ) =
     test source <|
         \_ ->
             let
+                input =
+                    List.map Byte.fromInt ints
+
                 ( result, machine ) =
-                    I.run source 10 []
+                    I.run source 15 input
             in
             case result of
                 Ok () ->
