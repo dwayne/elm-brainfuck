9d833d8384e453dad7c73641fdd45dfda6a137f3
Dwayne Crooks <me@dwaynecrooks.com>
2026-03-24 08:22:01 -0400

    Build a static UI with HTML/CSS

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Browser as B
 import Html as H
+import Html.Attributes as HA
 
 
 main : Program () Model Msg
@@ -35,4 +36,37 @@ update msg model =
 
 view : Model -> H.Html msg
 view _ =
-    H.text "Hello, world!"
+    H.div []
+        [ H.h1 [] [ H.text "Brainfuck" ]
+        , H.main_ [ HA.class "main" ]
+            [ H.div [ HA.class "input-area" ]
+                [ H.label [ HA.for "source" ] [ H.text "Source:" ]
+                , H.textarea
+                    [ HA.id "source"
+                    , HA.class "source"
+                    , HA.placeholder "Enter your program here..."
+                    ]
+                    []
+                , H.input
+                    [ HA.id "input"
+                    , HA.type_ "text"
+                    , HA.placeholder "Enter your input here..."
+                    ]
+                    []
+                , H.div []
+                    [ H.button
+                        [ HA.type_ "button"
+                        ]
+                        [ H.text "Run" ]
+                    ]
+                ]
+            , H.div [ HA.class "output-area" ]
+                [ H.label [ HA.for "output" ] [ H.text "Output:" ]
+                , H.output
+                    [ HA.id "output"
+                    , HA.for "source input"
+                    ]
+                    [ H.text "The output will appear here." ]
+                ]
+            ]
+        ]
