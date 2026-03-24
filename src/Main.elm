3a73384e0753396f70a9a10db21c3bb79cd90034
Dwayne Crooks <me@dwaynecrooks.com>
2026-03-24 08:58:17 -0400

    Get the run button working

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,12 @@
 module Main exposing (main)
 
+import Brainfuck.Data.Input as Input
+import Brainfuck.Interpreter as I
+import Brainfuck.Data.Machine as Machine
 import Browser as B
 import Html as H
 import Html.Attributes as HA
+import Html.Events as HE
 
 
 main : Program () Model Msg
@@ -15,27 +19,63 @@ main =
 
 
 type alias Model =
-    {}
+    { source : String
+    , input : String
+    , output : String
+    }
 
 
 init : Model
 init =
-    {}
+    { source = ">>,[>>,]<<[[<<]>>>>[<<[>+<<+>-]>>[>+<<<<[->]>[<]>>-]<<<[[-]>>[>+<-]>>[<<<+>>>-]]>>[[<+>-]>>]<]<<[>>+<<-]<<]>>>>[.>>]"
+    , input = "dacbe"
+    , output = ""
+    }
 
 
 type Msg
-    = NoOp
+    = EnteredSource String
+    | EnteredInput String
+    | ClickedRun
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        NoOp ->
-            model
+        EnteredSource s ->
+            { model | source = s }
+
+        EnteredInput s ->
+            { model | input = s }
+
+        ClickedRun ->
+            let
+                ( machine, result ) =
+                    --
+                    -- N.B. The input string is null terminated.
+                    --
+                    I.run model.source (Input.fromList [ Input.String model.input, Input.Int 0 ])
+            in
+            case result of
+                Ok () ->
+                    { model | output = Machine.toString machine }
+
+                Err err ->
+                    let
+                        output =
+                            case err of
+                                I.SyntaxError _ ->
+                                    "There was a syntax error."
+
+                                I.RuntimeError I.EndOfInput ->
+                                    "The end of the input was reached."
+                    in
+                    { model | output = output }
 
 
-view : Model -> H.Html msg
-view _ =
+
+view : Model -> H.Html Msg
+view { source, input, output } =
     H.div []
         [ H.h1 [] [ H.text "Brainfuck" ]
         , H.main_ [ HA.class "main" ]
@@ -45,17 +85,22 @@ view _ =
                     [ HA.id "source"
                     , HA.class "source"
                     , HA.placeholder "Enter your program here..."
+                    , HA.value source
+                    , HE.onInput EnteredSource
                     ]
                     []
                 , H.input
                     [ HA.id "input"
                     , HA.type_ "text"
                     , HA.placeholder "Enter your input here..."
+                    , HA.value input
+                    , HE.onInput EnteredInput
                     ]
                     []
                 , H.div []
                     [ H.button
                         [ HA.type_ "button"
+                        , HE.onClick ClickedRun
                         ]
                         [ H.text "Run" ]
                     ]
@@ -66,7 +111,11 @@ view _ =
                     [ HA.id "output"
                     , HA.for "source input"
                     ]
-                    [ H.text "The output will appear here." ]
+                    [ if String.isEmpty output then
+                        H.text "The output will appear here."
+                      else
+                        H.text output
+                    ]
                 ]
             ]
         ]
