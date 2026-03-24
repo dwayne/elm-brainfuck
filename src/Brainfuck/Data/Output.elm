d8eac3a137d31920df91eb7dfa1160f23afee714
Dwayne Crooks <me@dwaynecrooks.com>
2026-03-23 21:32:36 -0400

    Rewrite Output

---
+++ src/Brainfuck/Data/Output.elm
@@ -0,0 +1,22 @@
+module Brainfuck.Data.Output exposing (Output, empty, toString, writeByte)
+
+import Brainfuck.Data.Byte as Byte exposing (Byte)
+
+
+type Output
+    = Output (List Char)
+
+
+empty : Output
+empty =
+    Output []
+
+
+writeByte : Byte -> Output -> Output
+writeByte b (Output o) =
+    Output <| Byte.toChar b :: o
+
+
+toString : Output -> String
+toString (Output o) =
+    String.fromList <| List.reverse o
